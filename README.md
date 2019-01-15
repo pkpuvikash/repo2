dd8974221aed44c84b66356cff9e09e9e2da144f
Pkp <prashant.prasad@dxc.com>
2019-01-15 15:33:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# repo1
+# sampleproj
+# repo2
---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello {
+
+    public static void main(String[] args) {
+        int third=40;
+        int first = 10;
+        int second = 20;
+
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
